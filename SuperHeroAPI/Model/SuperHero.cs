ec003de285b49e3b820e0505cd6b0c7aec5bc85c
BK <fullstackbk@gmail.com>
2022-12-18 17:10:57 -0600

    Add project files.

---
+++ SuperHeroAPI/Model/SuperHero.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuperHeroAPI.Model
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; } = string.Empty;
+        [Required]
+        public string? FirstName { get; set; } = string.Empty ;
+        [Required]
+        public string? LastName { get; set; } = string.Empty;
+        [Required]
+        public string? Place { get; set; } = string.Empty;
+
+
+    }
+}
