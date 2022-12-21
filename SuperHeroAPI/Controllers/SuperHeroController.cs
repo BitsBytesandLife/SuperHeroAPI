99d7e00237391129d2a91f5d519863a2c08e1377
BK <fullstackbk@gmail.com>
2022-12-20 19:01:53 -0600

    all CRUD operations at hitting the database

--- SuperHeroAPI/Controllers/SuperHeroController.cs
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -8,33 +8,6 @@ namespace SuperHeroAPI.Controllers
     [ApiController]
     public class SuperHeroController : ControllerBase
     {
-        private static List<SuperHero> heroes = new List<SuperHero>()
-        {
-             new SuperHero
-             {
-                Id = 1,
-                Name = "Spider Man",
-                FirstName = "Peter",
-                LastName = "Parker",
-                Place = "New York City"
-             },
-              new SuperHero
-             {
-                Id = 2,
-                Name = "Iron Man",
-                FirstName = "Tony",
-                LastName = "Stark",
-                Place = "New York City"
-             },
-               new SuperHero
-             {
-                Id = 3,
-                Name = "Deadpool",
-                FirstName = "Wade",
-                LastName = "Wilson",
-                Place = "New York City"
-             },
-        };
         private readonly DataContext _context;
 
         public SuperHeroController(DataContext context)
@@ -87,11 +60,13 @@ namespace SuperHeroAPI.Controllers
         [HttpDelete("{id}")]
         public async Task<ActionResult<List<SuperHero>>> DeleteHero(int id)
         {
-            var currentHero = heroes.Find(h => h.Id == id);
+            var currentHero = await _context.SuperHeroes.FindAsync(id);
             if (currentHero == null)
                 return BadRequest("Hero not found");
-            heroes.Remove(currentHero);
-            return Ok(currentHero);
+           _context.SuperHeroes.Remove(currentHero);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.SuperHeroes.ToListAsync());
         }
 
 
